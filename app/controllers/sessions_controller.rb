a53974b402315c217bdc9abc3ad234f245520d80
n0tx <rcandra91@msn.com>
2025-05-10 21:26:02 +0700

    SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_or_create_by(email: params[:email]) do |u|
+      u.name = params[:name]
+    end
+    session[:user_id] = user.id
+    redirect_to dashboard_path
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
