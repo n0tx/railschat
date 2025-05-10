18ed105263d5c702bed70897aee7b106d182acbe
n0tx <rcandra91@msn.com>
2025-05-10 21:26:37 +0700

    RoomsController

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,27 @@
 class RoomsController < ApplicationController
+  before_action :require_login
+
   def index
+    @rooms = Room.all
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.create!(room_params)
+    redirect_to @room
   end
 
   def show
+    @room = Room.find(params[:id])
+    @messages = @room.messages
+    @message = Message.new
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
