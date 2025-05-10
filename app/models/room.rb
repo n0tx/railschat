b8beab23db2efe51ddca26c87e78754aa1d2f952
n0tx <rcandra91@msn.com>
2025-05-10 21:17:30 +0700

    Model associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,5 @@
 class Room < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  broadcasts_to ->(room) { room }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :messages, dependent: :nullify
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
 end
