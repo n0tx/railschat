e7d390e97e4be1b39db0f5814ae845a85e37fcb7
n0tx <rcandra91@msn.com>
2025-05-10 22:15:05 +0700

    Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def gravatar_url(email, size: 64)
+    hash = Digest::MD5.hexdigest(email.strip.downcase)
+    "https://www.gravatar.com/avatar/#{hash}?s=#{size}&d=identicon"
+  end
 end
